7bbb64ba06874b9c9825e486a122c8597662a5de
Eugene Vakulenko <jelezjz@gmail.com>
2022-04-10 21:15:14 +0300

    2 commit

---
+++ Task1.py
@@ -0,0 +1,10 @@
+f = open('Task1.txt', 'r', encoding='utf-8')
+text = f.read()
+f.close()
+ignore_symbol = '""''()/\;:<>.,!@#$%^&* -=+|`~{}[]\n'
+dic = {x.lower(): 0 for _, x in enumerate(text) if x not in ignore_symbol}
+for index, sum in enumerate(text):
+    if sum in dic:
+        dic[sum] += 1
+sorted = dict(sorted(dic.items(), key = lambda x: x[1], reverse = True))
+print(sorted)
