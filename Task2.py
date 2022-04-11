15f7969136de94c06b8a2dca6000770b925d4804
Eugene Vakulenko <jelezjz@gmail.com>
2022-04-11 17:37:45 +0300

    Task2

--- Task2.py
+++ Task2.py
@@ -1,14 +1,17 @@
 import hashlib
 import os
 from re import S
-files_dict = {}
-files = os.listdir('Task2')
-for file in files:
-    temp_file = open('./Task2/' + file)
-    hash = hashlib.md5(temp_file.read().encode('utf-8')).hexdigest()
-    if hash in files_dict:
-        files_dict[hash].append(file)
-    else:
-        files_dict[hash] = [file]
-    temp_file.close()
-print(files_dict)
+def main():
+    files_dict = {}
+    files = os.listdir('Task2')
+    for file in files:
+        temp_file = open('./Task2/' + file)
+        hash = hashlib.md5(temp_file.read().encode('utf-8')).hexdigest()
+        if hash in files_dict:
+            files_dict[hash].append(file)
+        else:
+            files_dict[hash] = [file]
+        temp_file.close()
+    print(files_dict)
+if __name__ == "__main__":
+    main()
