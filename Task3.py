ee27781af750a6359fe887460daaa190bd9d9df9
Eugene Vakulenko <jelezjz@gmail.com>
2022-04-12 14:35:25 +0300

    Task3

--- Task3.py
+++ Task3.py
@@ -1,8 +1,19 @@
 import os
-def main():
-    files = os.listdir('Task3')
-    with open('./Task3_copy/Task3.txt') as file:
-        for i, line in enumerate(file):
-            os.rename("Task3/" + files[i] + ".mp3", line)
-if __name__ == "__main__":
-    main()
+
+path = "./"
+basepath = os.path.abspath(os.path.dirname('Task3'))
+folder = os.path.join(basepath, 'Task3')
+
+with open(os.path.join(folder, 'Task3.txt'), 'r') as f:    
+    music_names: list = f.readlines() 
+
+music_names = [x.rstrip() for x in music_names]
+for i, music_name in enumerate(music_names):
+    music_names[i] = music_name[:music_name.rfind(':')] + '.' + music_name[music_name.rfind(':')+1:] + '.mp3'
+
+files = [f for f in os.listdir(folder) 
+        if os.path.isfile(os.path.join(folder, f)) and f.endswith('.mp3')]
+
+for file in files:
+    for name in music_names:
+        if file[:-4] in name: os.rename(os.path.join(folder, file), os.path.join(folder, name))
